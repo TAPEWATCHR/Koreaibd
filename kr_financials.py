e3010547a5e367f1a734eca9bb215ae82c44d358
TAPEWATCHR <hghgff@naver.com>
2026-06-07 11:45:04 +0900

    Fix import statement for OpenDartReader

--- kr_financials.py
+++ kr_financials.py
@@ -7,7 +7,7 @@ import datetime
 
 import pandas as pd
 import numpy as np
-from opendartreader import OpenDartReader
+import OpenDartReader
 
 DB_NAME = os.environ.get("KR_IBD_DB", "kr_ibd_system.db")
 DART_API_KEY = os.environ.get("DART_API_KEY", "")
